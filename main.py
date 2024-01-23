311efacde48e7dc12f1adfbfcef43bbeeeed2b68
ikari2k <pluta.m@gmail.com>
2024-01-23 16:25:45 +0100

    render titles of content in additional columns

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.jpeg")
 
 with col2:
-    st.title("Ardit Sulce")
+    st.title("Mateusz P.")
     content= """
-            In tempor cillum fugiat incididunt. Aliquip do sint ut ipsum incididunt nostrud nulla ullamco esse. 
-            Ullamco et nisi voluptate cupidatat. Commodo ad minim sunt eiusmod eiusmod fugiat. 
-            Occaecat duis enim nisi fugiat do occaecat exercitation.
+            I am Mateusz, python developer wannabe! 
             """
     st.info(content)
 
 content2 = """
-            Ipsum tempor occaecat id ex est eiusmod sunt irure in et. Proident laborum magna laborum ea laboris ex adipisicing consectetur ut reprehenderit. Dolor commodo ea dolor veniam. Consectetur eu nulla ex occaecat laborum irure officia ut esse exercitation dolor nostrud commodo irure. Labore sint aute enim laborum aliquip sunt deserunt incididunt ullamco non deserunt. Reprehenderit culpa amet et deserunt magna ea aliquip commodo elit minim minim. Deserunt nulla proident ea officia laboris ullamco est veniam cillum in.
-
-            Eiusmod fugiat est aliquip commodo irure ea dolore fugiat veniam labore officia. Consectetur et ut minim cillum deserunt non laboris eu minim mollit. Culpa ullamco Lorem duis culpa. Ex aliqua cillum dolor Lorem ex. Do ullamco aliquip dolore amet in. Proident consequat ipsum sunt eiusmod exercitation exercitation.
-
-            Duis irure in et veniam consectetur consectetur ea sunt anim aliquip amet voluptate magna qui. Officia esse Lorem nisi sunt. Exercitation esse officia quis est consectetur ad eiusmod tempor aliquip incididunt laborum.
+            Below you can find some of the apps I have built in Python!
             """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv('data.csv', sep=';')
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
